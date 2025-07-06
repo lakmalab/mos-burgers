b297139357babe1f8929ebf035694375c8dc50b7
lakma <lakmalaberathne@gmail.com>
2025-07-06 20:19:55 +0530

    added order form

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,18 @@
 console.log("hey burgers")
+document.getElementById('loginForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    const validUsername = 'admin';
+    const validPassword = '1234';
+
+    if (username === validUsername && password === validPassword) {
+        //alert('Login successful!');
+       window.location.href = 'assets/html/order.html';
+
+    } else {
+        alert('Invalid username or password');
+    }
+});
